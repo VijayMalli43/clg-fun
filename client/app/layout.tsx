02e9fcc28e9487f01307f4a8c26d3d5380bc8c22
crazy0codes <madhanpanja@gmail.com>
2025-03-10 16:09:19 +0530

    add initial client components and configuration for file upload and UI elements

---
+++ client/app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Simplifying Lab Assignment Submissions",
+  description: `
+  CrazyCodes.me is a dedicated platform designed to streamline the submission process for students' lab assignments. With a user-friendly interface and seamless functionality, students can securely send their lab work files directly to their college-provided email addresses or personal emails without any hassle. This ensures a smooth and efficient submission process, reducing the dependency on personal Gmail accounts or third-party services.
+  `,
+  generator: "crazyCodes.me",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <head>
+      <meta name="google-adsense-account" content="ca-pub-6049004127367436"></meta>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>crazycodes</title>
+        <link rel="icon" href="/favicon.ico" />
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
---
+++ client/app/page.tsx
@@ -0,0 +1,7 @@
+"use client"
+
+import FileUpload from "../file-upload"
+
+export default function SyntheticV0PageForDeployment() {
+  return <FileUpload />
+}
